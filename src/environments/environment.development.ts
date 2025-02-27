5424f55ebc36fbb91e82dc5757251f7db1f86210
Luke Silver <luke.silver@keyrus.co.za>
2025-02-27 22:50:53 +0200

    update env variable

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://web-law-firm-software-api-zan-gtbuhghxd7e2b4h7.southafricanorth-01.azurewebsites.net/'
+  apiUrl: 'https://web-law-firm-software-api-zan-gtbuhghxd7e2b4h7.southafricanorth-01.azurewebsites.net/api'
 };
