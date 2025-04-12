f7e7c46265b300662e4000c4c0763f7bdc401b81
Luke Silver <luke.silver@keyrus.co.za>
2025-04-12 14:06:36 +0200

    Working on the mobile navbar.

--- src/app/mobile-nav/mobile-nav.component.ts
+++ src/app/mobile-nav/mobile-nav.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-mobile-nav',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './mobile-nav.component.html',
-  styleUrl: './mobile-nav.component.css'
+  styleUrl: './mobile-nav.component.css',
+  animations: [
+    trigger('menuAnimation', [
+      state('void', style({ transform: 'translateY(-100%)', opacity: 0 })),
+      state('*', style({ transform: 'translateY(0)', opacity: 1 })),
+      transition('void => *', animate('300ms ease-in-out')),
+      transition('* => void', animate('300ms ease-in-out')),
+    ]),
+  ],
 })
 export class MobileNavComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(){
+    this.isMenuOpen = false;
+  }
 }
